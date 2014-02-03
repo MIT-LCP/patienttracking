1e601b7e2fb2aa733d70c18a290a14f8dafe614a
mghassem <mghassem@gmail.com>
2014-02-02 21:35:57 -0500

    Update lactacteExtraction.sql
    Updated the code to work properly and do two final selects depending on what you want.

--- lactacteExtraction.sql
+++ lactacteExtraction.sql
@@ -7,11 +7,24 @@ of type CSRU, and who has at least 5 lactate measurements. The
 variables being extracted are HR, MAP, urine output, and lactate.
 */
 
+-- ICD9 codes
+with CODEDATA AS (
+ SELECT C.SUBJECT_ID, C.HADM_ID, LISTAGG(C.CODE, ';') WITHIN GROUP (ORDER BY C.SEQUENCE) AS CODES
+ FROM MIMIC2V26.ICD9 C
+ GROUP BY C.SUBJECT_ID, C.HADM_ID
+ )
+ --select * from codedata;
+,
+
 -- Identify those with at least 5 lactate measures
-with minLact as(
-  select subject_id as pid  from (select subject_id, count(*) as LactN from mimic2v26.labevents 
-   where itemid=50010
-   group by subject_id)
+minLact as(
+  select subject_id as pid, hadm_id as hid  
+  from (
+      select subject_id, hadm_id, count(*) as LactN 
+      from mimic2v26.labevents 
+      where itemid=50010
+      group by subject_id, hadm_id
+      )
    where LactN > 5
 )
 --select * from minLact;-- where subject_id=12;
@@ -26,42 +39,33 @@ select *
   and subject_icustay_seq = 1
   and icustay_age_group = 'adult'
   and icud.subject_id = s.pid
+  and icud.hadm_id = s.hid
+  and s.hid is not null
   and ICUSTAY_FIRST_SERVICE = 'CSRU' 
 )
 --select * from cohort;
+--select count(distinct subject_id) from cohort; -- 525
+--select count(1) from cohort; -- 525
 , 
-
--- ICD9 codes
-code_data as (
-select i.SUBJECT_ID, LISTAGG(i.CODE, ';') WITHIN GROUP (ORDER BY i.CODE) AS CODES
- 
-  FROM MIMIC2V26.ICD9 i,
-       cohort c
-      
-WHERE c.SUBJECT_ID = i.SUBJECT_ID
-and c.hadm_id = i.hadm_id
-
-GROUP BY c.SUBJECT_ID
-)
-select * from CODE_DATA;
-,
 
 -- Flag patients with the following in the discharge summaries:
 --  'cabg' (coronary bypass graft), IABP  (intra aortic balloon pump), RUAD (right ventricular assistance device), 
 --   LUAD  (left ventricular assistance device)
 dis_Cond as (
-  select s.subject_id, 
+  select s.subject_id, --category,
     case when (lower(text) like '%cabg%' or lower(text) like '%coronary bypass graft%') then 1 else 0 end as CABG,
     case when (lower(text) like '%iabp%' or lower(text) like '%intra-aortic balloon pump%') then 1 else 0 end as IABP,
-    case when (lower(text) like '%ruad%' or lower(text) like '%right ventricular assistance device%') then 1 else 0 end as RUAD,
-    case when (lower(text) like '%luad%' or lower(text) like '%left ventricular assistance device') then 1 else 0 end as LUAD
+    case when (lower(text) like '%rvad%' or lower(text) like '%right ventricular assistance device%') then 1 else 0 end as RVAD,
+    case when (lower(text) like '%lvad%' or lower(text) like '%left ventricular assistance device%') then 1 else 0 end as LVAD
 
   from cohort s,
        mimic2v26.noteevents n
-  where n.icustay_id = s.icustay_id
-    and lower(n.category) like 'discharge summary'
+  where n.subject_id = s.subject_id
+    and n.hadm_id = s.hadm_id
+    and lower(n.category) like 'discharge_summary'
 )
-select * from dis_Cond;
+--select count(1) from dis_Cond where RVAD = 1; --3
+--select count(1) from dis_Cond where LVAD = 1; --4
 ,
 
 -- Pull out HR/MAP
@@ -137,19 +141,24 @@ LactateData as (
   select s.subject_id as subject_id, 
           c.valuenum as val, category, 
           c.charttime - s.icustay_intime as tm, c.charttime, 
-          s.icustay_intime, i.codes
+          s.icustay_intime, i.codes, d.IABP, d.CABG, d.LVAD, d.RVAD
           
     from cohort s
-    join CombinedParams c on c.subject_id = s.subject_id
-    join code_data i on i.subject_id = s.subject_id
-    
+    left join CombinedParams c on c.subject_id = s.subject_id
+    left join codedata i       on i.subject_id = s.subject_id
+    left join dis_Cond d       on d.subject_id = s.subject_id
+        
    where c.charttime >= s.icustay_intime
      and c.charttime - s.icustay_intime  <= INTERVAL '4' day 
       or c.charttime is null
       or c.category like '%SURVIVAL%'
       or c.category like '%LOS%'
-  )
-  
+     and i.hadm_id = s.hadm_id      
+)
+
+-- Select out the per-apatient attributed that are important
+select distinct subject_id, codes, IABP, CABG, LVAD, RVAD from LactateData order by subject_id;  
+,  
 -- Final selection formats the data into a time series format.
 select subject_id, category, val, 
        extract(day from tm) ||
@@ -157,3 +166,6 @@ select subject_id, category, val,
        ':' ||
        to_char(extract(minute from tm), 'FM00') as chtime from LactateData
        order by subject_id, tm, category;
+       
+       
+
