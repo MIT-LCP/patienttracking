f66517e82d0d661eeed081a4606652c08931ae47
mghassem <mghassem@gmail.com>
2014-03-04 15:22:01 -0500

    Delete exploreICD9.m

--- exploreICD9.m
+++
@@ -1,6 +0,0 @@
-% Load data
-names = {'fieldnames', 'sid', 'codes', 'iabp', 'cabg', 'lvad', 'rvad'};
-fields = ['subject_id, codes, iabp, cabg, lvad, rvad'];          
-
-t = importData(fields, names, 0, '\t', fullfile('~/patienttracking', 'lactatePatientData.csv'));
-v2struct(t);
